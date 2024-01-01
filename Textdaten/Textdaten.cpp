b4e322a311ae378edbb639c06999b38d35fe2291
jonas362123 <simonplo362@gmail.com>
2024-01-01 14:18:39 +0100

    read & print

--- Textdaten/Textdaten.cpp
+++ Textdaten/Textdaten.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+bool exitProgram;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    exitProgram = false;
+
+    std::fstream fs;
+    std::string fPath = "C:\\Users\\simon\\Desktop\\test.txt";
+    std::string content = "";
+    std::string line = "";
+    fs.open(fPath, std::ios_base::in);
+    while (std::getline(fs, line)) {
+        content += line += "\n";
+    }
+    fs.close();
+    
+    //std::cout << content; //print file content
+
+    while (!exitProgram) {
+        if (GetAsyncKeyState(VK_F1)) exitProgram = true;
+    }
+
+    return 0;
 }
