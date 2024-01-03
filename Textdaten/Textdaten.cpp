fa45e24cd7d0ced45ab1f29b39a43bbc9d98a697
jonas362123 <simonplo362@gmail.com>
2024-01-03 20:41:53 +0100

    count words & letters

--- Textdaten/Textdaten.cpp
+++ Textdaten/Textdaten.cpp
@@ -3,27 +3,58 @@
 #include <string>
 #include <windows.h>
 
+/*
+- Anzahl Wörter :)
+- Anzahl Buchstaben :)
+- Anzahl unterschiedlicher Wörter
+- durchschnitl. Länge/Wort
+*/
+
 bool exitProgram;
+bool showContent;
+int w_count;
+int b_count;
 
 int main()
 {
     exitProgram = false;
+    showContent = true;
+    w_count = 0;
+    b_count = 0;
 
     std::fstream fs;
-    std::string fPath = "C:\\Users\\simon\\Desktop\\test.txt";
+    std::string fPath = "C:\\test.txt"; // Pfad zur Datei
     std::string content = "";
     std::string line = "";
-    fs.open(fPath, std::ios_base::in);
-    while (std::getline(fs, line)) {
+
+    fs.open(fPath, std::ios_base::in); // Datei zur Auslesung öffnen
+    if (!fs.is_open())
+        std::cout << "failed to open file"; // Falls der Pfad nicht existiert oder die Datei nicht geöffnet werden kann
+
+    while (std::getline(fs, line)) { // fill content line by line
         content += line += "\n";
     }
-    fs.close();
-    
-    //std::cout << content; //print file content
+    fs.close(); // Importante :)
 
-    while (!exitProgram) {
-        if (GetAsyncKeyState(VK_F1)) exitProgram = true;
+
+    //TODO: Sonderzeichen in content entfernen
+    //std::isalnum()
+
+    for (int i = 0; i < content.size(); i++) { // loop through every character
+        if (content[i] == ' ' || content[i] == '\n') // check if character is a space or absatz thing
+            w_count += 1;
+        else
+            b_count += 1;
     }
 
-    return 0;
+    if (showContent)
+        std::cout << content << "\n";
+    std::cout << "Wordcount: " << std::to_string(w_count); // print amount of words
+    std::cout << "\nLettercount: " << std::to_string(b_count); // print amount of letters
+
+    while (!exitProgram) {
+        if (GetAsyncKeyState(VK_F1)) exitProgram = true; // only exit if key is pressed
+    }
+
+    return 0; // end of program obviously
 }
